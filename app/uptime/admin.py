83706bab2b6a8d8e43ddf582e09cf6e2f4737d7b
Sage Weil <sage@newdream.net>
2021-01-08 09:55:09 -0600

    Add models to django admin

--- app/uptime/admin.py
+++ app/uptime/admin.py
@@ -1,1 +1,22 @@
-# Register your models here.
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.Site)
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ("url", "active", "description", "owner")
+
+
+@admin.register(models.Check)
+class CheckAdmin(admin.ModelAdmin):
+    list_display = ("site", "state_up", "blocked", "ignore")
+
+
+@admin.register(models.Downtime)
+class DowntimeAdmin(admin.ModelAdmin):
+    list_display = ("site", "first_down_check", "last_down_check", "up_check")
+
+@admin.register(models.Proxy)
+class ProxyAdmin(admin.ModelAdmin):
+    list_display = ("source", "address", "description", "status")
