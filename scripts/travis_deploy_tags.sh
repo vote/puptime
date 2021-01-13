7d0d1f81087f33ee8197940812fb55344d6eee4d
Nick Catalano <nickcatal@gmail.com>
2021-01-13 13:10:30 -0500

    Code that deploys tags to ECR (hopefully)

--- scripts/travis_deploy_tags.sh
+++ scripts/travis_deploy_tags.sh
@@ -2,22 +2,22 @@
 
 pip install awscli
 
-eval $(aws ecr get-login --no-include-email --region us-west-2)
-docker build --cache-from voteamerica/puptime-ci-cache:latest --build-arg TAG_ARG=${TRAVIS_TAG} --build-arg BUILD_ARG=${TRAVIS_BUILD_NUMBER} -t puptime .
-docker tag puptime:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/puptime:${TRAVIS_TAG}
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/puptime:${TRAVIS_TAG}
+eval $(aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/e6b7u9l0)
+docker build --build-arg TAG_ARG=${TRAVIS_TAG} --build-arg BUILD_ARG=${TRAVIS_BUILD_NUMBER} -t puptime .
+docker tag puptime:latest public.ecr.aws/e6b7u9l0/puptime:${TRAVIS_TAG}
+docker push public.ecr.aws/e6b7u9l0/puptime/puptime:${TRAVIS_TAG}
 
 
-curl https://sentry.io/api/0/organizations/${SENTRY_ORG}/releases/ \
-  -X POST \
-  -H "Authorization: Bearer ${SENTRY_AUTH_TOKEN}" \
-  -H 'Content-Type: application/json' \
-  -d "
-{
-    \"version\": \"puptime@${TRAVIS_TAG}\",
-    \"refs\": [{
-        \"repository\":\"vote/puptime\",
-        \"commit\":\"${TRAVIS_COMMIT}\"
-    }],
-    \"projects\":[\"puptime\"]
-}"
+#curl https://sentry.io/api/0/organizations/${SENTRY_ORG}/releases/ \
+#  -X POST \
+#  -H "Authorization: Bearer ${SENTRY_AUTH_TOKEN}" \
+#  -H 'Content-Type: application/json' \
+#  -d "
+# {
+#    \"version\": \"puptime@${TRAVIS_TAG}\",
+#    \"refs\": [{
+#        \"repository\":\"vote/puptime\",
+#        \"commit\":\"${TRAVIS_COMMIT}\"
+#    }],
+#    \"projects\":[\"puptime\"]
+# }"
