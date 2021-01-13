c5c80833d2bd96d03ce1f061ac45b3a50d44b341
Sage Weil <sage@newdream.net>
2021-01-13 15:56:44 -0600

    Smaller client browser window size

--- app/uptime/selenium.py
+++ app/uptime/selenium.py
@@ -31,7 +31,7 @@ def get_driver(proxy):
     options.add_argument("--disable-gpu")
     options.add_argument("enable-automation")
     options.add_argument("--headless")
-    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--window-size=1024,1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-extensions")
     options.add_argument("--dns-prefetch-disable")
