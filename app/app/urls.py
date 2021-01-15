09bac0bebdd894e39dfaee6bb1925b22ba1622ac
Nick Catalano <nickcatal@gmail.com>
2021-01-15 17:47:20 -0500

    Add health endpoint

--- app/app/urls.py
+++ app/app/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from uptime.urls import urlpatterns as uptime_urlpatterns
 
 urlpatterns = [
+    path("-/", include("django_alive.urls")),
     path("admin/", admin.site.urls),
     path("v1/", include("app.api_urls")),
 ] + uptime_urlpatterns
