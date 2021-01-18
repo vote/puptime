fb714db1c51028716b5dad200b013ef70eb13668
Nick Catalano <nickcatal@gmail.com>
2021-01-18 14:43:18 -0500

    Use latest version of django
    (it's just easier at this point)

--- app/uptime/models.py
+++ app/uptime/models.py
@@ -1,4 +1,3 @@
-from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 from common import enums
@@ -9,7 +8,7 @@ class Site(UUIDModel, TimestampModel):
     url = models.TextField(null=True)
     active = models.BooleanField(default=True)
     description = models.TextField(null=True)
-    metadata = JSONField(null=True)  # type: ignore
+    metadata = models.JSONField(null=True)  # type: ignore
     owner = models.ForeignKey(
         "auth.User", related_name="sites", on_delete=models.CASCADE
     )
@@ -168,7 +167,7 @@ class Proxy(UUIDModel, TimestampModel):
     )
     failure_count = models.IntegerField(null=True)
     last_used = models.DateTimeField(null=True)
-    metadata = JSONField(null=True)  # type: ignore
+    metadata = models.JSONField(null=True)  # type: ignore
 
     class Meta:
         ordering = ["-created_at"]
